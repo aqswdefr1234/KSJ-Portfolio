59aedf066286e9d75306536fe0ab33836cc7f178
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-10-23 10:09:33 +0900

    Create MainPageJS.js

---
+++ MainPageJS.js
@@ -0,0 +1,18 @@
+document.getElementById('show-projects').addEventListener('click', function() {
+    var projectList = document.getElementById('project-list');
+    
+    // 프로젝트 정보를 추가합니다. 이 부분은 실제 프로젝트 정보로 대체해야 합니다.
+    projectList.innerHTML = `
+        <div class="project">
+            <img src="project1.png" alt="프로젝트 1 이미지">
+            <p>프로젝트 1에 대한 설명</p>
+        </div>
+        <div class="project">
+            <img src="project2.png" alt="프로젝트 2 이미지">
+            <p>프로젝트 2에 대한 설명</p>
+        </div>
+    `;
+    
+    // 프로젝트 목록을 표시합니다.
+    projectList.style.display = 'block';
+});
