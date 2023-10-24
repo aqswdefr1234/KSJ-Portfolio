18d6ae0bc2a6cafe4ee76c08627fbabebe55c9e7
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-10-24 10:58:21 +0900

    Update ProjectsPageJS.js

--- ProjectsPageFolder/ProjectsPageJS.js
+++ ProjectsPageFolder/ProjectsPageJS.js
@@ -1,27 +1,46 @@
-async function fetchData() {
-  const response = await fetch("ProjectsOrder.txt");
-  const data = await response.json();
-  return data
-}
+var topFolder = "/ProjectsPageFolder/"; // 실제 상위 폴더 경로로 변경하세요
 
-window.onload = function() {
-  fetchData();
-  var projectList = document.getElementById("project-list");
-  int projectCount = 2;
-  string projectsDiv = "";
-  
-  for(int i = 1; i <= projectCount; i++)
-  {
-    string explanation = "";
-    fetch('ProjectsFolder/Explanation.txt')
-    .then(response => response.text())
-    .then(data => {
-      projectsDiv += "
-        <div class="project">
-          <img src="Project/project${projectCount}.png">
-          <p>프로젝트 1에 대한 설명</p>
-        </div>
-        "
-    .catch(error => console.error(error));
-  }
-};
+    // HTML 요소를 가져오기
+    var txtContentElement = document.getElementById("txt-content");
+
+    // Txt 파일을 읽는 함수
+    function readTxtFile(txtFilePath) {
+        fetch(txtFilePath)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.text();
+            })
+            .then(txtContent => {
+                // 가져온 txt 내용을 HTML 요소에 추가
+                txtContentElement.textContent += txtContent;
+            })
+            .catch(error => {
+                console.error('Error fetching txt file:', error);
+            });
+    }
+
+    // 상위 폴더 내의 모든 txt 파일을 읽음
+    function readAllTxtFilesInTopFolder(topFolder) {
+        fetch(topFolder)
+            .then(response => response.text())
+            .then(data => {
+                // data는 상위 폴더 내의 내용을 나타냄
+                // 이 내용을 기반으로 txt 파일을 읽음
+                var lines = data.split('\n');
+                for (var i = 0; i < lines.length; i++) {
+                    var line = lines[i];
+                    if (line.endsWith(".txt")) {
+                        // txt 파일인 경우 읽음
+                        readTxtFile(topFolder + line);
+                    }
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching top folder:', error);
+            });
+    }
+
+    // 최상위 폴더에서 시작
+    readAllTxtFilesInTopFolder(topFolder);
