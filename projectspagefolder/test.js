78ce866b85c03b31463313750a9ad85a15a382f3
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-10-25 12:30:29 +0900

    Update test.js

--- projectspagefolder/test.js
+++ projectspagefolder/test.js
@@ -1,8 +1,8 @@
 // " / " 로 시작하면 루트 디렉토리부터시작하는 절대경로. " ./ " 는 현재 디렉토리인 상대경로.  " ../ " 는 상위 폴더
 // HTML 요소를 가져오기
 var txtContentElement = document.getElementById("txt-content");
-let txtOrder;
-let imageOrder;
+var txtOrder;
+var imageOrder;
 // Txt 파일을 읽는 함수
 function readTxtFile(index, txtOrder, FilePath) 
 {
@@ -61,14 +61,14 @@ function readAllTxtFilesInTopFolder() {
                 readImageFile(i, imageOrder, `${line[0]}/${line[2]}`);
                 }
             
-            let count = 0;
+            var count = 0;
             const intervalId = setInterval(() => {
                 console.log("반복 실행중: " + count);
                 count++;
             
                 // 원하는 조건에 도달하면 반복을 중지
-                let allTXTFilled = txtOrder.every(value => value !== "");
-                let allImageFilled = imageOrder.every(value => value !== "");
+                var allTXTFilled = txtOrder.every(value => value !== "");
+                var allImageFilled = imageOrder.every(value => value !== "");
                 if (allFilled == true) {//15초
                     console.log(txtOrder);
                     DivString(txtOrder, imageOrder);
@@ -87,7 +87,7 @@ function readAllTxtFilesInTopFolder() {
 }
 function DivString(txtOrder, imageOrder)
 {
-    let divString = "";
+    var divString = "";
     for(var i = 0; i < txtOrder.length; i++)
     {
         if(txtOrder[i] !== "null" && imageOrder[i] !== "null")
