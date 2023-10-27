304b688b29222b7ffeeb2e8ad5e0852095150caa
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-10-27 11:57:07 +0900

    Update test.js

--- projectspagefolder/test.js
+++ projectspagefolder/test.js
@@ -129,7 +129,7 @@ function DivString(txtOrder, imageOrder, linkUrlOrder)
 
         if(linkUrlOrder[i] !== "null")//null값이 아닌경우 링크 이미지 삽
         {
-            var url = linkUrlOrder[i].split(",")// , 없을 경우 첫번째 요소로 문자열이 배정된다.
+            var url = linkUrlOrder[i].split(",");// , 없을 경우 첫번째 요소로 문자열이 배정된다.
             for(var i = 0; i < url.length; i++)
             {
                 if(url[i].includes("GitHub:"))
