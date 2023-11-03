4d2a83384ee24b57da4df2fb896d4ae1feb4a74b
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-11-03 17:42:54 +0900

    Update Pyramid.js

--- projectspagefolder/Pyramid.js
+++ projectspagefolder/Pyramid.js
@@ -5,13 +5,24 @@ function ReadPyramin()
     fetch(Pyramid.txt)
         .then(response => response.text())
         .then(data => {
-            // 이 내용을 기반으로 txt 파일을 읽음
+            var divString = "";
+            var dict = {};
             var lines = data.split('\n');
             lines = lines.filter(line => line !== "");
-            var floor = lines[0].split("^^^");//첫번째 줄에는 층 정보 나옴
-            for(var i = 0; i < floor.length; i++)
+            var floors = lines[0].split("^^^");//첫번째 줄에는 층 정보 나옴
+            //lines.shift();//첫번째 요소 제거 후, 한칸 씩 앞으로
+            for(var j = 1; j < lines.length; j++)//첫번째 줄은 층에 대한 정보 이므로 제
             {
-                
+                var dictKey = lines[j].substring(0, lines[j].indexOf(":"));// : 전까지
+                var dictValue = lines[j].substring(lines[j].indexOf(":") + 1);// : 후부터
+                dict[dictKey] = dictValue;
+            }
+            
+            for(var i = 0; i < floors.length; i++)//층별로 쓰인 기술스택 이미지 주소찾기
+            {
+                var tech = floors[i].substring(0, floors[i].indexOf(":"));//기술스택이름
+                divString += dict[tech];
+                divString += "<br>";
             }
         })
         .catch(error => {
