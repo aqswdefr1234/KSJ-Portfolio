9757672ee2d9ddd4792ad817b3f813e11a3e466a
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-11-03 16:49:52 +0900

    Update Pyramid.js

--- projectspagefolder/Pyramid.js
+++ projectspagefolder/Pyramid.js
@@ -1,26 +1,20 @@
 var pyramid = document.getElementById("pyramid");
 
-var techStack = [
-    {tech: "JavaScript", img: "JavaScript.png"},
-    {tech: "HTML", img: "HTML.png"},
-    {tech: "CSS", img: "CSS.png"},
-    {tech: "Firebase", img: "Firebase.png"},
-    {tech: "Python", img: "Python.png"}
-];
-for (var i = 0; i < techStack.length; i++) {
-    var layer = document.createElement("div");
-    layer.style.textAlign = "center";
-    layer.style.marginBottom = "10px";
-    
-    for (var j = 0; j <= i; j++) {
-        var img = document.createElement("img");
-        img.src = techStack[i].img;
-        img.alt = techStack[i].tech;
-        img.style.width = "50px";
-        img.style.height = "50px";
-        img.style.margin = "5px";
-        layer.appendChild(img);
-    }
-    
-    pyramid.appendChild(layer);
+function ReadPyramin()
+{
+    fetch(Pyramid.txt)
+        .then(response => response.text())
+        .then(data => {
+            // 이 내용을 기반으로 txt 파일을 읽음
+            var lines = data.split('\n');
+            lines = lines.filter(line => line !== "");
+            var floor = lines[0].split("^^^");//첫번째 줄에는 층 정보 나옴
+            for(var i = 0; i < floor.length; i++)
+            {
+                
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching top folder:', error);
+        });
 }
