7cbfc76052f5f5e70c1e417808e54a77f4a149fc
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-11-04 13:43:40 +0900

    Update Pyramid.js

--- projectspagefolder/Pyramid.js
+++ projectspagefolder/Pyramid.js
@@ -21,7 +21,7 @@ async function ReadPyramin()//txt파일에는 항상 줄바꿈 문자 및 공백
         var techString = floors[i].substring(floors[i].indexOf(":") + 1);//기술스택이름
         var tech = techString.split(",").map(s => s.trim());
         console.log(`tech의 길이 ${i}번째 : ${tech.length}`);
-        divString += `<div class="section">`;
+        divString += `<div>`;
         for(var z = 0; z < tech.length; z++)
         {
             console.log(`${tech[z]} : ${dict[tech[z]]}`);
