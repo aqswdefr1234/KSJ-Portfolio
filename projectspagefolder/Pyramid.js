c62eb430eaf4ea96882a28f6acb879c9466025d5
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-11-08 14:54:02 +0900

    Update Pyramid.js

--- projectspagefolder/Pyramid.js
+++ projectspagefolder/Pyramid.js
@@ -9,6 +9,7 @@ function InsertDiv(dict, techArray, type)
         for(var z = 0; z < techArray.length; z++)
         {
             str += "<div>";
+            str += "<h3>ETC</h3>"
             str += dict[techArray[z]];
             str += "</div>";
         }
