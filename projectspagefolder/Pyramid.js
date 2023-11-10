57405b7918af03454acd038049edcb904e51b570
aqswdefr1234 <108164564+aqswdefr1234@users.noreply.github.com>
2023-11-10 13:05:59 +0900

    Update Pyramid.js

--- projectspagefolder/Pyramid.js
+++ projectspagefolder/Pyramid.js
@@ -11,8 +11,7 @@ function InsertDiv(dict, techArray, floorsCount, type)
         str += `<div style="height:${floorHeight}%">`;
         for(var z = 0; z < techArray.length; z++)
         {
-            str += "<div>";
-            //str += `<div style="width:${childWidth}%">`;
+            str += `<div style="width:${childWidth}%">`;
             str += dict[techArray[z]];
             str += "</div>";
         }
